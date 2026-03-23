7d4775d8f345058e63f38b95a6c8009f646220e3
day811 <day811@laposte.net>
2026-03-23 16:41:18 +0100

    add location field

--- scripts/init-multiple-databases.sh
+++ scripts/init-multiple-databases.sh
@@ -39,6 +39,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "sport_app_db" <<-'
     id text NOT NULL,
     employee_id int4 NULL,
     sport varchar(50) NULL,
+    location varchar(50) NULL,
     distance_meters int4 NULL,
     begin_date timestamp NULL,
     duration_sec int4 NULL,
